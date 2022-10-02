f7237c6c26b935b62a962b8a473886ecd793cf20
David <david.ojeda@nubank.com.br>
2022-10-01 21:34:41 -0600

    Set caption for images

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,39 @@
+const Image = require('@11ty/eleventy-img')
+const path = require("path")
+
+function imageShortCode(src, alt = "", captionClasses = "", imgClasses = "", sizes = "(min-width: 40em) 50vw, 100vw") {
+  const options = {
+    widths: [600, 640],
+    formats: ['webp', 'jpeg', 'gif'],
+    sharpOptions: {
+      animated: true
+    },
+    filenameFormat: ((id, src, width, format, options) => {
+      const extension = path.extname(src)
+      const name = path.basename(src, extension)
+      return `${name}-${width}w.${format}`
+    }),
+    urlPath: "/visuals_images/",
+    outputDir: "./_site/visuals_images",
+    useCache: true
+  }
+  Image(src, options)
+
+  const imageAttributes = {
+    class: imgClasses,
+    alt,
+    sizes,
+    loading: 'lazy',
+    decoding: 'async',
+  }
+
+  const metadata = Image.statsSync(src, options)
+  const html = Image.generateHTML(metadata, imageAttributes, { whitespaceMode: 'inline' })
+  return `<figure>${html}<figcaption class="${captionClasses}">${alt}</figcaption></figure>`
+
+}
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("visuals_images");
+  eleventyConfig.addPassthroughCopy("visuals_images")
+  eleventyConfig.addNunjucksShortcode("image", imageShortCode)
 }
